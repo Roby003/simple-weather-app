6d31ef29119d2f17149714e0b8b82d68f11f52a5
Roby003 <racsente@gmail.com>
2023-04-29 13:12:51 +0300

    completed weather in each hour for the next 10h

--- j.js
+++ j.js
@@ -1,6 +1,6 @@
 const apiUrl = "https://api.open-meteo.com/v1/forecast";
 const geoApi = 'https://nominatim.openstreetmap.org/search';
-
+const regex = /^[a-zA-Z]+$/;
 
 async function getTemperature(city, tUnit) {
     let lat = 0;
@@ -10,17 +10,32 @@ async function getTemperature(city, tUnit) {
             lat = data[0].lat
             lon = data[0].lon;
         })
-    fetch(`${apiUrl}?latitude=${lat}&longitude=${lon}&current_weather=true&temperature_unit=${tUnit}`)
+    await fetch(`${apiUrl}?latitude=${lat}&longitude=${lon}&current_weather=true&temperature_unit=${tUnit}&timezone=auto&hourly=temperature_2m&`)
         .then(response => response.json())
         .then(data => {
             console.log(data);
             const temperatureDiv = document.getElementById("temp");
             const temperatureData = data.current_weather.temperature;
-           console.log(tUnit);
             if (tUnit === 'celsius')
                 temperatureDiv.innerText = `${temperatureData}°C`;
             else
                 temperatureDiv.innerText = `${temperatureData}F`;
+
+            const index = data.hourly.time.indexOf(data.current_weather.time);
+            const divs = document.getElementsByClassName('temp2');
+            const divs2 = document.getElementsByClassName('time');
+            if (tUnit === 'celsius') {
+                for (let i = 0; i < divs.length; i++) {
+                    divs[i].innerText = `${data.hourly.temperature_2m[index + i + 1]}°C`;
+                    divs2[i].innerText = data.hourly.time[index + i + 1].slice(-5);
+                }
+            }
+            else {
+                for (let i = 0; i < divs.length; i++) {
+                    divs[i].innerText = `${data.hourly.temperature_2m[index + i + 1]}F`;
+                    divs2[i].innerText = data.hourly.time[index + i + 1].slice(-5);
+                }
+            }
         })
         .catch(error => {
             console.error(error);
@@ -31,18 +46,30 @@ function submitCity() {
     event.preventDefault();
     let inputElement = document.getElementById("city-name");
     let city = inputElement.value;
+    if(regex.test(city))
     updateDegreeType(city);
-
-}
-function updateDegreeType(city) {
-    var selectElement = document.getElementById("degreeSelect");
-
-
-    var degreeType = selectElement.value;
-
-    getTemperature(city, degreeType);
-
+    else
+        alert("city name not valid");
 }
 
+async function updateDegreeType(city) {
+    let selectElement = document.getElementById("degreeSelect");
+    let degreeType = selectElement.value;
+    await getTemperature(city, degreeType);
+    changeEmoji();
 
+}
+async function changeEmoji() {
+    const divs = document.getElementsByClassName('card');
+    for (let i = 0; i < divs.length; i++) {  
+        if (divs[i].getElementsByClassName('temp2')[0].innerText > '15') {
+            divs[i].classList.remove('ice');
+            divs[i].classList.add('hot');
+        }
+        else {
+            divs[i].classList.remove('hot');
+            divs[i].classList.add('ice');
+        }
+    }
 
+}
